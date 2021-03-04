2c074e5c2b09bc670aba82ae51f7b95780696507
Pavel Alyamovski <moby76@mail.ru>
2021-03-05 00:45:30 +0700

    Добавлена надпись Изготовление и продажа мягкой мебели

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           {/* Edit <code>src/App.js</code> and save to reload. */}
-          АртДиван
+          АртДиван: Изготовление и продажа мягкой мебели
         </p>
         <a
           className="App-link"
