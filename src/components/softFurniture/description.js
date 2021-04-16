9ef6214d8fde72c81823c0702de9e5e1b3ccd169
Pavel Alyamovski <moby76@mail.ru>
2021-04-16 22:49:53 +0700

    Изменения касаемые замены &nbsp; на \u00A0

--- src/components/softFurniture/description.js
+++ src/components/softFurniture/description.js
@@ -3,14 +3,15 @@ import { Link } from 'react-router-dom'
 
 function Description({ modelDesc, modelName, id }) {
     return (
-        <>
-            <h5>описание модели <Link to={`/models/${id}`} target="_blank" className="text-info" >{modelName}</Link></h5>
-            <div className="row">
+        // <>
+            <div className="container">
+                <h5>описание модели <Link to={`/models/${id}`} target="_blank" className="text-info" >{modelName}</Link></h5>
+
                 {modelDesc.split('\n').map((item, i) => {
                     return <p className="mb-1" key={i}>{item}</p>
                 })}
             </div>
-        </>
+        // </>
     )
 }
 
--- src/components/softFurniture/features.js
+++ src/components/softFurniture/features.js
@@ -3,11 +3,16 @@ import React from 'react'
 function Features({ features }) {
     return (
         <>
+            
+            <div className="container">
             <h5>Особенности товара</h5>
-            <div className="row">
                 {features ?
-                    features.value.replace(/(<([^>]+)>)/ig, "").split('\n').map((item, i) => {
-                        return <p className="mb-1" key={i}>{item}</p>
+                    features.value.replace(/&nbsp;/g, '\u00A0\u00A0\u00A0\u00A0').replace(/(<([^>]+)>)/ig, "").split('\n').map((item, i) => {
+                        return (
+                            <>
+                                <p className="mb-1" key={i}>{item}</p>                               
+                            </>
+                        )
                     })
                     :
                     <p>Данный товар не имеет отличительных особенностей от базовой модели</p>
